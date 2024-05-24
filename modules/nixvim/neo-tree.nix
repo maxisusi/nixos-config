1e908abd25635bc9e56fe31b7291b21ea86a1b09
maxisusi <maxbalej@proton.me>
2024-05-24 12:36:25 +0200

    Enabling diagnostics and git support

--- modules/nixvim/neo-tree.nix
+++ modules/nixvim/neo-tree.nix
@@ -2,6 +2,14 @@
   plugins.neo-tree = {
     enable = true;
     enableRefreshOnWrite = true;
+    enableGitStatus = true;
+    enableDiagnostics = true;
+    gitStatusAsync = true;
+    gitStatusAsyncOptions = {
+      batchSize = 1000;
+      batchDelay = 10;
+      maxLines = 10000;
+    };
     filesystem.followCurrentFile = {
       enabled = true;
     };
