552b50fef41db71ed8abe211ef854d1a4f9c6668
maxisusi <maxbalej@proton.me>
2024-05-24 22:39:06 +0200

    Adding keymaps for lsp

--- modules/nixvim/lsp.nix
+++ modules/nixvim/lsp.nix
@@ -3,19 +3,41 @@
     lsp = {
       enable = true;
       servers = {
+        # Javascript
         tsserver.enable = true;
-        nixd.enable = true;
         eslint.enable = true;
+        nixd.enable = true;
         zls.enable = true;
         cmake.enable = true;
         phpactor.enable = true;
+        rust-analyzer = {
+          installCargo = true;
+          installRustc = true;
+          enable = true;
+        };
       };
-      keymaps.lspBuf = {
-        "gd" = "definition";
-        "gD" = "references";
-        "gt" = "type_definition";
-        "gi" = "implementation";
-        "K" = "hover";
+      keymaps = {
+        lspBuf = {
+          "gd" = "definition";
+          "gD" = "references";
+          "gt" = "type_definition";
+          "gi" = "implementation";
+          "K" = "hover";
+        };
+        extra = [
+          {
+            key = "<leader>lr";
+            action = "<CMD>LspRestart<Enter>";
+          }
+          {
+            key = "<leader>lx";
+            action = "<CMD>LspStop<Enter>";
+          }
+          {
+            key = "<leader>ls";
+            action = "<CMD>LspStart<Enter>";
+          }
+        ];
       };
     };
     lsp-lines = {
