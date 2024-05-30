fe64c7debb720e3f2d0807fe67337937d515174e
maxisusi <maxbalej@proton.me>
2024-05-30 13:16:12 +0200

    Packagin indent-blankline on another file

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -27,43 +27,13 @@
     ./friendly-snippets.nix
     ./luasnip.nix
     ./dressing.nix
+    ./indent-blankline.nix
   ];
 
-  extraPlugins = with pkgs.vimPlugins; [
-    { plugin = indent-blankline-nvim; }
-    { plugin = nvim-window-picker; }
-  ];
+  extraPlugins = with pkgs.vimPlugins; [{ plugin = nvim-window-picker; }];
   extraConfigLua =
     #lua
     ''
-      -- Highlight rainbow for indentation
-      local highlight = {
-          "RainbowRed",
-          "RainbowYellow",
-          "RainbowBlue",
-          "RainbowOrange",
-          "RainbowGreen",
-          "RainbowViolet",
-          "RainbowCyan"
-      }
-      local hooks = require "ibl.hooks"
-      hooks.register(
-          hooks.type.HIGHLIGHT_SETUP,
-          function()
-              vim.api.nvim_set_hl(0, "RainbowRed", {fg = "#E06C75"})
-              vim.api.nvim_set_hl(0, "RainbowYellow", {fg = "#E5C07B"})
-              vim.api.nvim_set_hl(0, "RainbowBlue", {fg = "#61AFEF"})
-              vim.api.nvim_set_hl(0, "RainbowOrange", {fg = "#D19A66"})
-              vim.api.nvim_set_hl(0, "RainbowGreen", {fg = "#98C379"})
-              vim.api.nvim_set_hl(0, "RainbowViolet", {fg = "#C678DD"})
-              vim.api.nvim_set_hl(0, "RainbowCyan", {fg = "#56B6C2"})
-          end
-      )
-
-      vim.g.rainbow_delimiters = {highlight = highlight}
-      require("ibl").setup {scope = {highlight = highlight}}
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
       -- Add borders to the hover and diagnostics
       local _border = "single"
       vim.lsp.handlers["textDocument/hover"] =
