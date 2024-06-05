5fa08de491d72a7bc6050a24bc92f7522e90e5b2
maxisusi <maxbalej@proton.me>
2024-06-05 19:03:10 +0200

    [Tipee] Adding git hooks and autologin file on project if not present

--- shells/tipee/default.nix
+++ shells/tipee/default.nix
@@ -18,6 +18,16 @@ in pkgs.mkShell {
   shellHook = ''
     npm config set prefix $TMPDIR # Store the global dir in a temporary folder to install global npm packages
     npm i -g @styled/typescript-styled-plugin typescript-styled-plugin
+    # Check if prepare-commit-msg is inside tipee on .git/hooks
+    if [ ! -f /home/max/Documents/dev/tipee/.git/hooks/prepare-commit-msg ]; then
+      cp ./prepare-commit-msg /home/max/Documents/dev/tipee/.git/hooks/prepare-commit-msg 
+    fi
+
+    # Add auto login user id to .env.local
+    if [ ! -f /home/max/Documents/dev/tipee/.env.local ]; then
+      echo "AUTO_LOGIN_USER_ID=1" > /home/max/Documents/dev/tipee/.env.local
+    fi
+
     tmuxifier load-session ./tipee-run.sh # Load tipee session
   '';
 }
