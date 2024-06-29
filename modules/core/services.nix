90b9ad8b33d5daf535cf95bf7bc2cd29db9c51e1
maxisusi <maxbalej@proton.me>
2024-06-29 11:20:08 +0200

    Adding brother printer drivers

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,7 +1,14 @@
-{
+{ pkgs, ... }: {
   services.libinput = {
     touchpad.naturalScrolling = true;
     mouse.naturalScrolling = true;
   };
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.brlaser ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+  };
 }
