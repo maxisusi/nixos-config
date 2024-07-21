6411f600925de418c2a1985ce77f461c0a6d6775
maxisusi <maxbalej@proton.me>
2024-07-21 17:52:08 +0200

    Reverting to plasma 5

--- modules/core/xserver.nix
+++ modules/core/xserver.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     #  -- KDE SETTINGS --
-    desktopManager.plasma6.enable = true;
+    desktopManager.plasma5.enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
