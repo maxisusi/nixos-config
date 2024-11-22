7bb5ea3e6030bb0893880d50b66f4967537523a2
maxisusi <maxbalej@proton.me>
2024-11-22 11:50:01 +0100

    Cleaning up hyrland config

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, config, lib, ... }: {
+{ config, lib, ... }: {
 
   imports = [ (import ./hardware.nix) ] ++ [ (import ./user.nix) ]
     ++ [ (import ./pipewire.nix) ] ++ [ (import ./system.nix) ]
@@ -10,29 +10,4 @@
     desktop.enable = (lib.mkIf (config.networking.hostName == "desktop") true);
     laptop.enable = (lib.mkIf (config.networking.hostName == "laptop") true);
   };
-
-  programs.hyprland = {
-    enable = true;
-    # set the flake package
-    package =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-
-    xwayland.enable = true;
-
-    systemd.setPath.enable = true;
-  };
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys =
-      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
-  services.hypridle.enable = true;
-  programs.hyprlock.enable = true;
-
-  # programs.uwsm.enable = true;
-
 }
--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -1,4 +1,4 @@
-{
+{ inputs, pkgs, ... }: {
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
@@ -6,4 +6,26 @@
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "max" ];
   };
+
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+
+    xwayland.enable = true;
+
+    systemd.setPath.enable = true;
+  };
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
+  services.hypridle.enable = true;
+  programs.hyprlock.enable = true;
 }
