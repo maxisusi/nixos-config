c11c19f87cb8a00f6e2524069212391f57d50e9b
maxisusi <maxbalej@proton.me>
2025-01-16 08:42:30 +0100

    Adding hyprlock and hypridle, but not configured yet

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -157,6 +157,34 @@ in {
     hyprlock
   ];
 
+  programs.hyprlock = {
+    enable = true;
+    settings = { };
+  };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+
+      listener = [
+        {
+          timeout = 900;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
+
   services.hyprpaper = {
     enable = true;
     settings = {
