dc09f22ef1afb6d5ab4e112de4c4c564d715b91c
maxisusi <maxbalej@proton.me>
2025-04-10 12:29:03 +0000

    feat(fish): adding docker command aliases

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -9,6 +9,9 @@
       # GENERALS
       ee = "exit";
       dps = "docker ps";
+      dcu = "docker compose up";
+      dcd = "docker compose down";
+      dcr = "docker compose restart";
       vi = "nvim";
       ns = "nix-shell --command fish";
       s = "kitten ssh";
