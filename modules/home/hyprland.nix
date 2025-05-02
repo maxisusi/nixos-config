c23c51074f387919833ab67ba6e2ac6b93fc35d5
maxisusi <maxbalej@proton.me>
2025-05-02 15:12:54 +0200

    fix(hypr|sys): remove hyprlock settings + change sessionVariable FLAKE

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -181,61 +181,7 @@ in {
   programs.rofi.enable = true;
   services.dunst.enable = true;
 
-  programs.hyprlock = {
-    enable = true;
-    settings = {
-
-      background = {
-        path = "screenshot";
-
-        blur_passes = 2;
-        blur_size = 7;
-
-        brightness = 0.8;
-        contrast = 0.8;
-
-        # color = "${base00}99";
-      };
-
-      input-field = {
-        size = {
-          width = 200;
-          height = 50;
-        };
-
-        outline_thickness = 3;
-        dots_size = 0.33;
-        dots_spacing = 0.15;
-        dots_center = false;
-        # outer_color = "${base01}";
-        # inner_color = "${base07}";
-        # font_color = "${base00}";
-        fade_on_empty = true;
-        placeholder_text = "<i>Type Password...</i>";
-        hide_input = false;
-        position = {
-          x = 0;
-          y = -20;
-        };
-        halign = "center";
-        valign = "center";
-      };
-
-      label = {
-        text = "$TIME";
-        # color = "${base05}";
-        font_size = 50;
-        font_family = "Noto Sans";
-        position = {
-          x = 0;
-          y = 80;
-        };
-        halign = "center";
-        valign = "center";
-      };
-    };
-  };
-
+  programs.hyprlock = { enable = true; };
   services.hypridle = {
     enable = true;
     settings = {
