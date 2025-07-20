bd3f757bfe90b9929ed3efbf905ebf5f88710905
maxisusi <maxbalej@proton.me>
2025-07-20 07:34:40 +0200

    feat(update): system

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -55,44 +55,44 @@
     tomato-c
   ];
 
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      max = {
-        bookmarks = {
-          force = true;
-          settings = [
-            {
-              name = "wikipedia";
-              tags = [ "wiki" ];
-              keyword = "wiki";
-              url =
-                "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-            }
-            {
-              name = "kernel.org";
-              url = "https://www.kernel.org";
-            }
-            {
-              name = "Nix sites";
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "homepage";
-                  url = "https://nixos.org/";
-                }
-                {
-                  name = "wiki";
-                  tags = [ "wiki" "nix" ];
-                  url = "https://wiki.nixos.org/";
-                }
-              ];
-            }
-          ];
-        };
-      };
-    };
-  };
+  # programs.firefox = {
+  #   enable = true;
+  #   profiles = {
+  #     max = {
+  #       bookmarks = {
+  #         force = true;
+  #         settings = [
+  #           {
+  #             name = "wikipedia";
+  #             tags = [ "wiki" ];
+  #             keyword = "wiki";
+  #             url =
+  #               "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+  #           }
+  #           {
+  #             name = "kernel.org";
+  #             url = "https://www.kernel.org";
+  #           }
+  #           {
+  #             name = "Nix sites";
+  #             toolbar = true;
+  #             bookmarks = [
+  #               {
+  #                 name = "homepage";
+  #                 url = "https://nixos.org/";
+  #               }
+  #               {
+  #                 name = "wiki";
+  #                 tags = [ "wiki" "nix" ];
+  #                 url = "https://wiki.nixos.org/";
+  #               }
+  #             ];
+  #           }
+  #         ];
+  #       };
+  #     };
+  #   };
+  # };
 
   services.udiskie = {
     enable = true;
