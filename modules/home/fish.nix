abddc341d4ee23452c1457ecfce1d59d23d5f015
maxisusi <maxbalej@proton.me>
2025-10-04 22:04:56 +0200

    correctly setting up direnv with fish

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -4,6 +4,9 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       zoxide init fish | source
+
+      direnv hook fish | source
+      set -g direnv_fish_mode eval_on_arrow
     '';
     shellAliases = {
       # GENERALS
