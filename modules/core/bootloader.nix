f8acda317c538d7e78899ef22ef44aa2a4df4c84
maxisusi <maxbalej@proton.me>
2026-03-07 09:08:52 +0100

    bootloader changes

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -3,4 +3,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.unstable.linuxPackages_zen;
+
+  # Disable the boot entry editor for security (prevents kernel parameter tampering).
+  boot.loader.systemd-boot.editor = false;
 }
